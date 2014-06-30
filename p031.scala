f9adf8163b46665244d50d3099c953133211d29c
Stewart Stewart <stewinsalot@gmail.com>
2014-06-30 14:25:19 -0700

    Remove repeated parameters from Problem 031 solutions (scala and python) for readability

--- p031.scala
+++ p031.scala
@@ -8,12 +8,12 @@ How many different ways can 2f be made using any number of coins?
 */
 
 
-def countChangeWays(amount: Int, coins: Int*): Int =
+def countChangeWays(amount: Int, coins: Array[Int]): Int =
   if (amount < 0 || coins.length == 0) 0
   else if (amount == 0) 1
-  else (countChangeWays(amount, coins.drop(1): _*) +
-    countChangeWays(amount - coins(0), coins: _*))
+  else (countChangeWays(amount, coins.drop(1)) +
+    countChangeWays(amount - coins(0), coins))
 
-val ans = countChangeWays(200, 200, 100, 50, 20, 10, 5, 2, 1)
+val ans = countChangeWays(200, Array(200, 100, 50, 20, 10, 5, 2, 1))
 
 println(ans)
