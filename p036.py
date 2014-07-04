3a7db049b09029f00721e6ee7e1cfce2867e830f
Stewart Stewart <stewinsalot@gmail.com>
2014-07-04 00:40:32 -0700

    Cleaned up problem 36 solutions and drafted an explanation.

--- p036.py
+++ p036.py
@@ -1,17 +1,8 @@
-"""The decimal number, 585 = 10010010012 (binary), is palindromic in
-both bases.
-
-Find the sum of all numbers, less than one million, which are
-palindromic in base 10 and base 2.
-
-(Please note that the palindromic number, in either base, may not
-include leading zeros.)"""
-
 from itertools import count, takewhile
 LIMIT = 10**6
 
 
-def decimalPalindromes():
+def decimal_palindromes():
     for i in count(1):
         for n in range(10**(i-1), 10**i):
             yield int(str(n) + str(n)[-2::-1])
@@ -24,7 +15,7 @@ def bin_is_pali(n):
     return binstr == binstr[::-1]
 
 
-ans = sum(filter(bin_is_pali,
-                 takewhile(lambda n: n < LIMIT, decimalPalindromes())))
+ans = sum(filter(bin_is_pali, takewhile(lambda n: n < LIMIT,
+                                        decimal_palindromes())))
 
 print(ans)
--- p036.scala
+++ p036.scala
@@ -1,34 +1,19 @@
-/**The decimal number, 585 = 10010010012 (binary), is palindromic in
-both bases.
-
-Find the sum of all numbers, less than one million, which are
-palindromic in base 10 and base 2.
-
-(Please note that the palindromic number, in either base, may not
-include leading zeros.)*/
-
 val LIMIT = math.pow(10, 6).toInt
 
+def merge (a: Stream [Int], b: Stream [Int]): Stream [Int] = 
+  if (a.head<b.head) a.head #:: merge(a.tail, b)
+  else b.head #:: merge(b.tail, a)
 
-def decimalPalindromes(): Stream[Int] = {
-  def powersOfTen: Stream[Int] = Stream.cons(1, powersOfTen map (_*10))
-  def palindromesByMagnitude(mag: Int): Stream[String] = {
-    def evenLenPali(n: Int) = n.toString + n.toString.reverse
-    def oddLenPali(n: Int) = n.toString + n.toString.reverse.substring(1)
-    val nums = (mag until mag*10).toStream
-
-    (nums map oddLenPali) ++ (nums map evenLenPali)
-  }
-
-  (powersOfTen map palindromesByMagnitude).flatten map Integer.parseInt
-}
-
+def decimalPalindromes(): Stream[Int] = merge (
+  Stream from 1 map (n =>
+    Integer.parseInt((n/10).toString + n.toString.reverse)),
+  Stream from 1 map (n =>
+    Integer.parseInt(n.toString + n.toString.reverse)))
 
 def binIsPali(n: Int) = {
   val binstr = Integer.toBinaryString(n)
   binstr equals binstr.reverse
 }
-
 
 val ans = decimalPalindromes.takeWhile(_ < LIMIT).filter(binIsPali(_)).sum
 
