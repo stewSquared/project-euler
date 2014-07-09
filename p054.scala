120d16da15deec24d9197b35914e2db23ad60d21
Stewart Stewart <stewinsalot@gmail.com>
2014-07-08 19:04:53 -0700

    Problem 54: Gave Scala solution a main method

--- p054.scala
+++ p054.scala
@@ -1,74 +1,73 @@
 import Function.tupled
 
-class PokerHand (val cards: Array[String]) extends Ordered[PokerHand] {
+object p054 {
+  class Hand (val cards: Array[String]) extends Ordered[Hand] {
+    val CARD_VALUES = "23456789TJQKA".toArray
+    val HAND_RANKS = Array (
+      "HighCard",
+      "OnePair",
+      "TwoPair",
+      "ThreeKind",
+      "Straight",
+      "Flush",
+      "FullHouse",
+      "FourKind",
+      "StraightFlush")
 
-  val HAND_RANKS = Array {
-    "HighCard"
-    "OnePair"
-    "TwoPair"
-    "ThreeKind"
-    "Straight"
-    "Flush"
-    "FullHouse"
-    "FourKind"
-    "StraightFlush"
-  }
+    val values = for (c <- cards) yield (CARD_VALUES.indexOf(c(0)) + 1)
+    val suits = for (c <- cards) yield c(1)
 
-  val CARD_VALUES = "23456789TJQKA".toArray
+    lazy val rank: Int = HAND_RANKS.indexOf {
+      val distinct = values.toSet.size
+      lazy val longest = (
+        for ((_, group) <- values.groupBy(identity)) yield group.length
+      ).max
 
-  val values = for (c <- cards) yield (CARD_VALUES.indexOf(c(0)) + 1)
-  val suits = for (c <- cards) yield c(1)
+      distinct match {
+        case 2 => if (longest == 4) "FourKind" else "FullHouse"
+        case 3 => if (longest == 3) "ThreeKind" else "TwoPair"
+        case 4 => "OnePair"
+        case 5 => {
+          val straight = values.max - values.min == 4
+          val flush = suits.toSet.size == 1
+          if (straight && flush) "StraightFlush"
+          else if (straight) "Straight"
+          else if (flush) "flush"
+          else "HighCard"
+        }
+      }
+    }
 
-  lazy val rank = HAND_RANKS.indexOf {
-    val distinctValues = values.toSet.size
-    lazy val largestGroupSize = (
-      for ((_, group) <- values.groupBy(identity)) yield group.length
-    ).max
+    override def compare(that: Hand): Int = {
+      def tiebreakers(values: Array[Int]) = {
+        def groupScore(value: Int, group: Array[Int]) =
+          value * math.pow(CARD_VALUES.length+1, group.length-1).toInt
 
-    distinctValues match {
-      case 2 => if (largestGroupSize == 4) "FourKind" else "FullHouse"
-      case 3 => if (largestGroupSize == 3) "ThreeKind" else "TwoPair"
-      case 4 => "OnePair"
-      case 5 => {
-        val straight = values.max - values.min == 4
-        val flush = suits.toSet.size == 1
-        if (straight && flush) "StraightFlush"
-        else if (straight) "Straight"
-        else if (flush) "flush"
-        else "HighCard"
+        (values groupBy identity map tupled(groupScore)).toArray.sorted.reverse
+      }
+      if (this.rank == that.rank) {
+        val (thisTiebreaker, thatTiebreaker) =
+          (tiebreakers(this.values) zip tiebreakers(that.values)
+            dropWhile tupled(_ == _)).head
+
+        thisTiebreaker - thatTiebreaker
+      } else {
+        this.rank - that.rank
       }
     }
   }
 
-  def compare(that: PokerHand) = {
-    def tieBreakers(values: Array[Int]): Array[Int] = {
-      def groupScore(value: Int, group: Array[Int]) =
-        value * math.pow(CARD_VALUES.length+1, group.length-1).toInt
+  def main(args: Array[String]) {
+    val filename = if (args.nonEmpty) args(0) else "p054.txt"
 
-      (values groupBy identity map tupled(groupScore)).toArray.sorted.reverse
-    }
+    val t0 = System.nanoTime / 1000000
+    val ans = (io.Source.fromFile(filename).getLines map (_.split(' '))
+      map (cards => (new Hand(cards.slice(0,5)), new Hand(cards.slice(5,10))))
+      filter tupled(_ > _)).length
+    val t1 = System.nanoTime / 1000000
 
-    if (this.rank - that.rank == 0) {
-      val (thisTieBreaker: Int, thatTieBreaker: Int) =
-        (tieBreakers(this.values) zip tieBreakers(that.values)
-          dropWhile tupled(_ == _)).head
+    println(t1 - t0)
 
-      thisTieBreaker - thatTieBreaker
-    } else {
-      this.rank - that.rank
-    }
+    println(ans)
   }
 }
-
-def roundHands(round: String) = {
-  val cards = round.split(' ')
-  (new PokerHand(cards.slice(0,5)), new PokerHand(cards.slice(5,10)))
-}
-
-val ans = (
-  io.Source.fromFile("p054.txt").getLines
-    map roundHands
-    filter tupled(_ > _)
-).length
-
-println(ans)
