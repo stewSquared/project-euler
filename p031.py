20d812cf141c33f7f4d77810b7aec88c1d5192c4
Stewart Stewart <stewinsalot@gmail.com>
2014-07-08 19:08:23 -0700

    minor housecleaning

--- p031.py
+++ p031.py
@@ -1,13 +1,3 @@
-"""In England the currency is made up of pound, f, and pence, p, and
-there are eight coins in general circulation:
-
-    1p, 2p, 5p, 10p, 20p, 50p, 1f (100p) and 2f (200p).
-
-How many different ways can 2f be made using any number of coins?
-
-"""
-
-
 def count_change_ways(amount, coins):
     return (0 if amount < 0 or not coins else
             1 if amount == 0 else
--- p031.scala
+++ p031.scala
@@ -1,13 +1,3 @@
-/**In England the currency is made up of pound, f, and pence, p, and
-there are eight coins in general circulation:
-
-    1p, 2p, 5p, 10p, 20p, 50p, 1f (100p) and 2f (200p).
-
-How many different ways can 2f be made using any number of coins?
-
-*/
-
-
 def countChangeWays(amount: Int, coins: Array[Int]): Int =
   if (amount < 0 || coins.length == 0) 0
   else if (amount == 0) 1
