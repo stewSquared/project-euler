8dffde9d9243d34a6d98317c242aa95c7cd9d7d9
Stewart Stewart <stewinsalot@gmail.com>
2014-07-08 19:03:21 -0700

    Problem 54: Copied scala solution's tiebreaker to improve python solution

--- p054.py
+++ p054.py
@@ -1,5 +1,7 @@
-from itertools import groupby, starmap, count
+import sys, time
+from itertools import *
 
+CARD_VALUES = list("23456789TJQKA")
 HAND_RANKS = [
     "HighCard",
     "OnePair",
@@ -11,64 +13,59 @@ HAND_RANKS = [
     "FourKind",
     "StraightFlush"]
 
-CARD_VALUES = list("23456789TJQKA")
 
 class Hand:
-
     def __init__(self, cards):
         self.cards = cards
-        self.values = [c[0] for c in cards]
+        self.values = [CARD_VALUES.index(c[0])+1 for c in cards]
         self.suits = [c[1] for c in cards]
 
+    @property
     def rank(self):
         distinct = len(set(self.values))
-        clumpSize = max(len(list(g)) 
-                        for k, g in groupby(sorted(self.values)))
+        longest = max(len(list(g)) 
+                      for k, g in groupby(sorted(self.values)))
         if distinct == 2:
-            rank = "FourKind" if clumpSize == 4 else "FullHouse"
+            rank = "FourKind" if longest == 4 else "FullHouse"
         elif distinct == 3:
-            rank = "ThreeKind" if clumpSize == 3 else "TwoPair"
+            rank = "ThreeKind" if longest == 3 else "TwoPair"
         elif distinct == 4:
             rank = "OnePair"
         else:
             flush = len(set(self.suits)) == 1
-            run = sorted(CARD_VALUES.index(c) for c in self.values)
-            straight = run[-1] - run[0] == 4
+            straight = max(self.values) - min(self.values) == 4
             rank = ("StraightFlush" if straight and flush
                     else "Straight" if straight 
                     else "Flush" if flush
-                    else "HighCard") 
-        assert rank in HAND_RANKS
-        return rank
-
+                    else "HighCard")
+        return HAND_RANKS.index(rank)
+ 
     def trumps(self, other):
-        def outranks(self, other):
-            return (HAND_RANKS.index(self.rank()) > 
-                    HAND_RANKS.index(other.rank()))
+        def tiebreakers(values):
+            def group_score(value, group):
+                return (value * (len(CARD_VALUES)+1) ** (len(list(group))-1))
 
-        def tiebreakers(self):
-            """Return a list of cards values with which to break ties."""
-            if self.rank() in ["HighCard", "Flush"]:
-                return sorted(map(lambda c: CARD_VALUES.index(c),
-                                  self.values),
-                              reverse=True)
-            else:
-                groups = sorted([(CARD_VALUES.index(k), len(list(g)))
-                                 for k, g in groupby(sorted(self.values,
-                                                            reverse=True))],
-                                key=lambda tup: tup[1], # tup = (value, size)
-                                reverse=True)
-                return list(starmap((lambda value, size: value), groups))
+            return reversed(sorted(starmap(group_score,
+                                           groupby(sorted(values)))))
+        if self.rank == other.rank:
+            selfTiebreaker, otherTiebreaker = next(dropwhile(
+                lambda tup: tup[0] == tup[1],
+                zip(tiebreakers(self.values), tiebreakers(other.values))
+            ))
+            return selfTiebreaker > otherTiebreaker
+        else:
+            return self.rank > other.rank
 
-        return (self.outranks(other) or
-                not other.outranks(self) and
-                self.tiebreakers() > other.tiebreakers())
 
-def matchHands(match):
-    cards = match.split()
-    return (Hand(cards[0:5]), Hand(cards[5:10]))
+if __name__ == "__main__":
+    filename = sys.argv[1] if len(sys.argv) > 1 else "p054.txt"
 
-ans = len(list(filter(None, starmap(lambda left, right: left.trumps(right),
-                                    map(matchHands, open("p054.txt"))))))
+    t0 = int(time.time()*1000)
+    ans = list(starmap(lambda left, right: left.trumps(right),
+                       map(lambda cards: (Hand(cards[0:5]), Hand(cards[5:10])),
+                           (line.split() for line in open(filename))))).count(1)
+    t1 = int(time.time()*1000)
 
-print(ans)
+    print(t1 - t0)
+
+    print(ans)
